caef599ecaf5dfca32c8a4961d7520edbb92ebbe
Ivan Suslov <ioankingisepp@mail.ru>
2018-09-08 00:02:20 +0300

    Исправление личного проекта в соответствии с критериями

--- js/main.js
+++ js/main.js
@@ -38,138 +38,137 @@ var storage = {
     feedback_email: "",
 };
 
-  try {
-    storage.feedback_name = localStorage.getItem("feedback_name", feedback_name.value);
-    storage.feedback_email = localStorage.getItem("feedback_email", feedback_email.value);
-  } catch (err) {
-    isStorageSupport = false;
-  }
+try {
+  storage.feedback_name = localStorage.getItem("feedback_name", feedback_name.value);
+  storage.feedback_email = localStorage.getItem("feedback_email", feedback_email.value);
+} catch (err) {
+  isStorageSupport = false;
+}
 
-  if ( link_write_us ) {
-    link_write_us.addEventListener("click", function (evt) {
+if ( link_write_us ) {
+  link_write_us.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    overlay.classList.add("active");
+    popup_write_us.classList.add("active");
+    feedback_name.focus();
+
+    if ( storage.feedback_name ) {
+      feedback_name.value = storage.feedback_name;
+      feedback_email.focus;
+    } else if ( storage.feedback_name ) {
+      feedback_email.value = storage.feedback_email;
+      feedback_text.focus;
+    }
+  });
+}
+
+if ( link_buy ) {
+  // link_buy.forEach(function(item) {
+  for (var item = 0; item < link_buy.length; item++) {
+    link_buy[item].addEventListener("click", function(evt) {
       evt.preventDefault();
       overlay.classList.add("active");
-      popup_write_us.classList.add("active");
-      feedback_name.focus();
-
-      if ( storage.feedback_name ) {
-        feedback_name.value = storage.feedback_name;
-        feedback_email.focus;
-      } else if ( storage.feedback_name ) {
-        feedback_email.value = storage.feedback_email;
-        feedback_text.focus;
-      }
+      popup_bookmarks.classList.add("active");
     });
-  }
+  };
+}
 
-  if ( link_buy ) {
-    // link_buy.forEach(function(item) {
-    for (var item = 0; item < link_buy.length; item++) {
-      link_buy[item].addEventListener('click', function(evt) {
-        evt.preventDefault();
-        console.log('Клик');
-        overlay.classList.add("active");
-        popup_bookmarks.classList.add("active");
-      });
-    };
-  }
+if ( link_map ) {
+  link_map.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    overlay.classList.add("active");
+    popup_map.classList.add("active");
+  });
+}
 
-  if ( link_map ) {
-    link_map.addEventListener("click", function (evt) {
+if ( close_write_us ) {
+  close_write_us.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    overlay.classList.remove("active");
+    popup_write_us.classList.remove("active");
+    popup_write_us.classList.remove("modal-error");
+    popup_write_us.offsetWidth = popup_write_us.offsetWidth;
+    popup_write_us.classList.remove("modal-error");
+  });
+}
+
+if ( close_map ) {
+  close_map.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    overlay.classList.remove("active");
+    popup_map.classList.remove("active");
+  });
+}
+
+if ( close_bookmarks ) {
+  close_bookmarks.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    overlay.classList.remove("active");
+    popup_bookmarks.classList.remove("active");
+  });
+}
+
+if ( form_write_us ) {
+  form_write_us.addEventListener("submit", function (evt) {
+    if ( !feedback_name.value || !feedback_email.value ) {
       evt.preventDefault();
-      overlay.classList.add("active");
-      popup_map.classList.add("active");
-    });
-  }
-
-  if ( close_write_us ) {
-    close_write_us.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      overlay.classList.remove("active");
-      popup_write_us.classList.remove("active");
-      popup_write_us.classList.remove("modal-error");
-      popup_write_us.offsetWidth = popup_write_us.offsetWidth;
-      popup_write_us.classList.remove("modal-error");
-    });
-  }
-
-  if ( close_map ) {
-    close_map.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      overlay.classList.remove("active");
-      popup_map.classList.remove("active");
-    });
-  }
-
-  if ( close_bookmarks ) {
-    close_bookmarks.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      overlay.classList.remove("active");
-      popup_bookmarks.classList.remove("active");
-    });
-  }
-
-  if ( form_write_us ) {
-    form_write_us.addEventListener("submit", function (evt) {
-      if ( !feedback_name.value || !feedback_email.value ) {
-        evt.preventDefault();
-        popup_write_us.classList.add("modal-error");
-      } else {
-        if (isStorageSupport) {
-          localStorage.setItem("feedback_name", feedback_name.value);
-          localStorage.setItem("feedback_email", feedback_email.value);
-        }
-      }
-    });
-  }
-
-  if ( button_back ) {
-    button_back.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      if ( !tab_punchers.checked ) {
-        tab_punchers.checked = true;
-        tab_punchers.classList.add("active");
-      }
-      if (tab_drills.classList.contains("active")) {
-        tab_drills.classList.remove("active");
-      }
-    });
-  }
-
-  if ( button_next ) {
-    button_next.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      if ( !tab_drills.checked ) {
-        tab_drills.checked = true;
-        tab_drills.classList.add("active");
-      }
-      if (tab_punchers.classList.contains("active")) {
-        tab_punchers.classList.remove("active");
-      }
-    });
-  }
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if ( popup_map ) {
-        if (popup_map.classList.contains("active")) {
-          popup_map.classList.remove("active");
-        }
-      }
-      if ( popup_write_us ) {
-        if (popup_write_us.classList.contains("active")) {
-          popup_write_us.classList.remove("active");
-          popup_write_us.classList.remove("modal-error");
-        }
-      }
-      if ( popup_bookmarks ) {
-        if (popup_bookmarks.classList.contains("active")) {
-          popup_bookmarks.classList.remove("active");
-        }
-      }
-      if (overlay.classList.contains("active")) {
-        overlay.classList.remove("active");
+      popup_write_us.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("feedback_name", feedback_name.value);
+        localStorage.setItem("feedback_email", feedback_email.value);
       }
     }
   });
+}
+
+if ( button_back ) {
+  button_back.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if ( !tab_punchers.checked ) {
+      tab_punchers.checked = true;
+      tab_punchers.classList.add("active");
+    }
+    if (tab_drills.classList.contains("active")) {
+      tab_drills.classList.remove("active");
+    }
+  });
+}
+
+if ( button_next ) {
+  button_next.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if ( !tab_drills.checked ) {
+      tab_drills.checked = true;
+      tab_drills.classList.add("active");
+    }
+    if (tab_punchers.classList.contains("active")) {
+      tab_punchers.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if ( popup_map ) {
+      if (popup_map.classList.contains("active")) {
+        popup_map.classList.remove("active");
+      }
+    }
+    if ( popup_write_us ) {
+      if (popup_write_us.classList.contains("active")) {
+        popup_write_us.classList.remove("active");
+        popup_write_us.classList.remove("modal-error");
+      }
+    }
+    if ( popup_bookmarks ) {
+      if (popup_bookmarks.classList.contains("active")) {
+        popup_bookmarks.classList.remove("active");
+      }
+    }
+    if (overlay.classList.contains("active")) {
+      overlay.classList.remove("active");
+    }
+  }
+});
